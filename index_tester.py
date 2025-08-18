a0c9d95ba6c5fa2bc50ffb56765ccc49c719c979
Megan <meganthielman@gmail.com>
2025-08-17 23:08:22 -0700

    Added in additional Unit Tests

--- index_tester.py
+++ index_tester.py
@@ -12,5 +12,40 @@ class TestActions(unittest.TestCase):
         self.assertEqual("rope" in hero_hat_dict["Cowboy hat"], True)
         self.assertEqual("flower" in hero_hat_dict["Cowboy hat"], False)
 
+    def test_del_hero_hat_dict(self):
+        new_hero = index.Hero()
+
+        new_hero.add_to_hat_dict("Cowboy hat", ["rope"])
+        new_hero.del_hero_hat_dict("Cowboy hat")
+
+        self.assertFalse(new_hero.get_hero_hat_dict())
+
+    def test_del_hat_dict(self):
+        new_hero = index.Hero()
+        new_hero.del_hat_dict("Cowboy hat")
+
+        self.assertTrue("Cowboy hat" not in new_hero.get_hat_dict())
+
+    def test_update_health(self):
+        new_hero = index.Hero()
+
+        self.assertTrue(new_hero.get_health(),40)
+        new_hero.update_health(-10)
+        self.assertTrue(new_hero.get_health(),30)
+
+    def test_del_hero_hat_dict_item(self):
+        new_hero = index.Hero()
+
+        new_hero.add_to_hat_dict("Cowboy hat", ["rope", "cow"])
+        new_hero.del_hero_hat_dict_item("Cowboy hat", "cow")
+        hero_hat_dict = new_hero.get_hero_hat_dict()
+
+        self.assertTrue("cow" not in hero_hat_dict["Cowboy hat"])
+
+
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
